0ac5267bee86416990cd5c13169d4f9fb759d8c1
CLF <liangfu127@163.com>
2019-05-31 15:36:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+gpus = '6,7'
+batch_size = 8
+num_cls = 200
+INPUT_SIZE = (448, 448)
+PROPOSAL_NUM = 6
+STEPS = [60, 100]
+CAT_NUM = 4
+initial_learning_rate = 0.001
+weight_decay = 1e-4
+save_weights_path = 'models/cub200'
+dataset_dir = '/mnt/sdf/clf8113/datasets/CUB_200_2011'
