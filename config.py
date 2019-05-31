3d72f2a7faf964a01cfdb0855306f8308245c03d
CLF <liangfu127@163.com>
2019-05-31 15:37:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-gpus = '6,7'
-batch_size = 8
+gpus = '0,1,2,3'
+batch_size = 16
 num_cls = 200
 INPUT_SIZE = (448, 448)
 PROPOSAL_NUM = 6
@@ -8,4 +8,4 @@ CAT_NUM = 4
 initial_learning_rate = 0.001
 weight_decay = 1e-4
 save_weights_path = 'models/cub200'
-dataset_dir = '/mnt/sdf/clf8113/datasets/CUB_200_2011'
+dataset_dir = 'datasets/CUB_200_2011'
