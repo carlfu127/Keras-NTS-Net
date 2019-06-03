f9a4a8f7e47b3f53e4b5cd258a8770a651c03c8a
CLF <liangfu127@163.com>
2019-06-03 13:39:57 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -13,28 +13,25 @@ def partloss(PROPOSAL_NUM, num_cls):
     return part
 
 def rankloss(PROPOSAL_NUM, num_cls):
-    # def rankloss_fn(y_true, y_pred):
-    ####y_pred = N*pro*num_cls
     def rank(y_true, y_pred):
-        # batch_size, PROPOSAL_NUM, num_cls = K.int_shape(y_pred)
+        ####y_pred = N*pro*(num_cls+1)
         y_pred = K.reshape(y_pred, (-1, PROPOSAL_NUM, num_cls+1))
         part_prob = -K.log(K.reshape(y_pred[..., :-1], (-1, num_cls))) ##N*pro, *num_cls
         label = K.cast(K.expand_dims(y_true, axis=1), tf.int32)
-        targets = K.reshape(K.repeat_elements(label, PROPOSAL_NUM, axis=1), (-1, num_cls))
-        # temp = K.softmax(part_logits, axis=-1)
-        target_label = tf.argmax(targets, axis=-1, output_type=tf.int32)
+        label = K.reshape(K.repeat_elements(label, PROPOSAL_NUM, axis=1), (-1, num_cls)) ##N*pro, *num_cls
+        target_label = tf.argmax(label, axis=-1, output_type=tf.int32)  ##N*pro
         indice = K.stack([tf.range(tf.shape(target_label)[0]), target_label], axis=-1)
         part_pred = tf.gather_nd(part_prob, indice)
-        part_pred = K.reshape(part_pred, (-1, PROPOSAL_NUM))
-        top_n_prob= y_pred[..., -1]
+        part_pred = K.reshape(part_pred, (-1, PROPOSAL_NUM)) ##N*pro
+        top_n_prob= y_pred[..., -1] ##N*pro
 
         loss = 0.
-        y_pred = top_n_prob
-        y_true = part_pred
+        score = top_n_prob
+        targets = part_pred
         for i in range(PROPOSAL_NUM):
-            targets_p = K.cast((y_true > K.expand_dims(y_true[:, i], axis=1)), tf.float32)
-            pivot = K.expand_dims(y_pred[:, i], axis=1)
-            loss_p = (1 - pivot + y_pred) * targets_p
+            targets_p = K.cast((targets > K.expand_dims(targets[:, i], axis=1)), tf.float32)
+            pivot = K.expand_dims(score[:, i], axis=1)
+            loss_p = (1 - pivot + score) * targets_p
             loss_p = K.relu(loss_p)
             loss += loss_p
         return K.mean(loss)
